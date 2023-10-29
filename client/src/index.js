40c9cb33939e5bfc17ff9ebaaba48300db2ff69c
Aakash <vermaaakash1000@gmail.com>
2023-10-29 19:51:31 +0530

    wrapping Providers of Chakra and Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider} from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+import { ColorModeScript } from '@chakra-ui/react'
+
+import { Provider } from 'react-redux';
+import store from './redux/store.js'
+
+
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: false,
+    initialColorMode: "dark",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
 
