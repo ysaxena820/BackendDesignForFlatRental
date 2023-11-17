68307042b863badf5e452518483ac921c5351b24
Aakash <vermaaakash1000@gmail.com>
2023-11-17 18:26:23 +0530

    Navbar updated

--- client/src/Pages/Navbar.jsx
+++ client/src/Pages/Navbar.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { AiOutlineUser } from 'react-icons/ai'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import Dropdown from '../components/Dropdown';
 function Navbar() {
-    const navItems = ['Home', 'For Rent', 'For Sell', 'Forum', 'About', 'login']
+
+    const { token, username } = useSelector((state) => state.auth);
+    const [callToggleDropdown, setCallToggleDropdown] = useState(0);
+
+    const navItems = ['Home', 'Rent', 'Publish', 'Forum', 'About']
+    const authToken = localStorage.getItem('token')
+    const authUsername = localStorage.getItem('user')
+    const [loggedIn, setLoggedIn] = useState(false);
+    useEffect(() => {
+        setLoggedIn(!loggedIn);
+
+
+    }, [token, authToken, authUsername])
+
+
+
+
     return (
-        <div className='nav flex bg-[#FFFFFF29] w-[550px] h-[52px] rounded-3xl font-jost mt-10 justify-center gap-8 items-center text-[16px]'>
-            {navItems.map((item) => {
-                return (<div>{item}</div>)
+        <nav className='nav flex bg-[#FFFFFF29] w-[550px] h-[52px] rounded-3xl font-jost mt-10 justify-center gap-8 items-center text-[16px]'>
+            {navItems.map((item, index) => {
+                return (<NavLink key={index} className='transition ease-in-out delay-150 duration-200 hover:scale-110 hover:text-cyan-500' to={`${item == 'Home' ? '/' : item}`}>{item}</NavLink>)
             })}
-        </div>
+            {
+                authToken && authUsername ?
+                    (
+                        <>
+                            <button
+                                onClick={() => {
+                                    setCallToggleDropdown((callToggleDropdown) => callToggleDropdown + 1)
+                                }}
+                                className='transition ease-in-out delay-150 duration-200 hover:scale-110 hover:text-cyan-500 flex items-center gap-1' to='/login'><AiOutlineUser />{authUsername}</button>
+                            <Dropdown callToggleDropdown={callToggleDropdown} />
+                        </>
+                    )
+                    :
+                    <NavLink className='transition ease-in-out delay-150 duration-200 hover:scale-110 hover:text-cyan-500 flex items-center gap-1' to='/login'><AiOutlineUser />Login</NavLink>
+            }
+        </nav>
     )
 }
 
