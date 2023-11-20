bff3baecc375873fcb18a1e35364952e4ea9b920
Aakash <vermaaakash1000@gmail.com>
2023-11-20 18:48:05 +0530

    Forum Replies added

--- client/src/Pages/Replies.jsx
+++ client/src/Pages/Replies.jsx
@@ -1,14 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Replies = () => {
     const [reply, setReply] = useState("");
+    const [replyList, setReplyList] = useState([]);
+    const [title, setTitle] = useState("");
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    const addReply = () => {
+        fetch("http://localhost:3002/create/reply", {
+            method: "POST",
+            body: JSON.stringify({
+                id,
+                userName: localStorage.getItem("user"),
+                reply,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                alert(data.message);
+                navigate("/forum");
+            })
+            .catch((err) => console.error(err));
+    };
 
     const handleSubmitReply = (e) => {
         e.preventDefault();
         console.log({ reply });
+        addReply();
         setReply("");
     };
 
+    useEffect(() => {
+        const fetchReplies = () => {
+            fetch("http://localhost:3002/thread/replies", {
+                method: "POST",
+                body: JSON.stringify({
+                    id,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setReplyList(data.replies);
+                    setTitle(data.title);
+                })
+                .catch((err) => console.error(err));
+        };
+        fetchReplies();
+    }, [id]);
     return (
         <main className='replies'>
             <form className='modal__content' onSubmit={handleSubmitReply}>
@@ -24,6 +70,16 @@ const Replies = () => {
 
                 <button className='modalBtn'>SEND</button>
             </form>
+            <div className='thread__container'>
+                {replyList.map((reply) => (
+                    <div className='thread__item'>
+                        <p>{reply.text}</p>
+                        <div className='react__container'>
+                            <p style={{ opacity: "0.5" }}>by {reply.name}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </main>
     );
 };
