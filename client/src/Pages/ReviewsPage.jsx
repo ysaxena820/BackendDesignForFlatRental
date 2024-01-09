5620e8004503ceabf69911a15504b090df77bc64
Aakash <vermaaakash1000@gmail.com>
2024-01-09 14:33:56 +0530

    reviews loading state added

--- client/src/Pages/ReviewsPage.jsx
+++ client/src/Pages/ReviewsPage.jsx
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Input, Button, Textarea, VStack, Flex } from '@chakra-ui/react';
+import { FaSpinner } from "react-icons/fa";
 import { toast } from 'react-toastify';
 
 const ReviewsPage = ({ productId, onReviewSubmit }) => {
     const [message, setMessage] = useState('');
     const [reviewList, setReviewList] = useState(null);
+    const [loading, setLoading] = useState(false)
     const { id } = useParams();
     console.log(id)
 
@@ -16,6 +18,7 @@ const ReviewsPage = ({ productId, onReviewSubmit }) => {
         fetchReviewData();
     }, [id])
     const fetchReviewData = async () => {
+        setLoading(true)
         const response = await axios.post('https://flat-apartment.onrender.com/reviews/getById', {
             id
         });
@@ -26,13 +29,13 @@ const ReviewsPage = ({ productId, onReviewSubmit }) => {
         else {
             console.log('Failed to fetch');
         }
+        setLoading(false)
     }
     const handleSubmit = async () => {
         if (!localStorage.getItem("user")) {
             toast.error("Please login before submitting a review!")
         }
         else {
-
             try {
                 // Send review data to the backend
                 const response = await axios.post('https://flat-apartment.onrender.com/reviews/submit', {
@@ -54,19 +57,20 @@ const ReviewsPage = ({ productId, onReviewSubmit }) => {
     return (
         <div className='flex flex-col h-full justify-between'>
             <div className='w-full min-h-[10rem] pt-2 pd-2 scroll-container'>
-                {reviewList ? (
-                    reviewList.map((review, index) => (
-                        <div>
-                            <div key={index} className='flex justify-between pr-2 pl-2 gap-2'>
-                                <p className='text-sm'>{review.message}</p>
-                                <p className='text-xs text-gray-300'>{review.username}</p>
-                            </div>
-                            <hr className='mb-2 mt-1' />
-                        </div>
-                    ))
-                ) : (
-                    <p>No Reviews here...</p>
-                )}
+                {loading ? <FaSpinner className='animate-spin bg-transparent text-3xl mt-auto' /> :
+                    (reviewList?.length > 0 ?
+                        (
+                            reviewList.map((review, index) => (
+                                <div>
+                                    <div key={index} className='flex justify-between pr-2 pl-2 gap-2'>
+                                        <p className='text-sm'>{review.message}</p>
+                                        <p className='text-xs text-gray-300'>{review.username}</p>
+                                    </div>
+                                    <hr className='mb-2 mt-1' />
+                                </div>
+                            ))
+                        ) : <p>No Reviews here... </p>)
+                }
             </div>
             <div className='flex gap-2 '>
                 <Input
