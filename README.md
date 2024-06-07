ec56c0c4f7495afdd2dcfe97f1e6b03903998129
Yash Saxena <ysaxena820@gmail.com>
2024-06-07 12:03:54 +0530

    For display purpose exposing the backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# Flat-Apartment-Rent
-A repo for booking flats or pg's personalized for university students.
--- client/src/styles/theme.js
+++
@@ -1,7 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-export const theme = extendTheme({
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-});
-export default theme;
